3887e91423a4e587d21239666601c1ad884e8ca7
Edgard <eins.yeager@gmail.com>
2024-06-27 13:06:59 +0500

    mysql init with typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/test.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '47.237.78.128',
+      port: 8080,
+      username: 'user',
+      password: 'user',
+      database: 'user',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {}
+}
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/test.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User) private readonly User: Repository<User>,
+  ) {}
+  async getHello(): Promise<User[]> {
+    await this.User.save({ firstName: 'test' });
+    return await this.User.find();
   }
 }
