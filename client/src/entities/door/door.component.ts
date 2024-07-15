b6f33ca05bafe308db0d4e9702ea506ae2fe911c
Edgard <eins.yeager@gmail.com>
2024-07-15 12:59:26 +0500

    door input

--- client/src/entities/door/door.component.ts
+++ client/src/entities/door/door.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FavoriteSvgComponent } from '../../shared/icons/favorite.component';
 import { CompareSvgComponent } from '../../shared/icons/compare.component';
 
@@ -10,8 +10,8 @@ import { CompareSvgComponent } from '../../shared/icons/compare.component';
   styleUrl: './door.component.scss',
 })
 export class DoorComponent {
-  img = '';
-  title = 'Polka';
-  text =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at tincidunt nisi. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at tincidunt nisi. Aliquam erat volutpat.';
+  @Input({ required: true }) img = '';
+  @Input({ required: true }) props = '';
+  @Input({ required: true }) title = '';
+  @Input({ required: true }) text = '';
 }
